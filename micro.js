b82ca6a8493c4c6aa1f1e6f530c212f04a1a8f34
vkpbm <kanchanswami8178@gmail.com>
2026-02-10 18:47:10 +0530

    second

--- micro.js
+++ micro.js
@@ -1,13 +1,19 @@
 
 
-let a=5
-let b=4
+let age=5;
+// let b=4
 
-console.log(" a**b =",a**b)
-const student ={
-    name:"nancy",
-    age: 28,
-    cgpa :8.5,
-    ispass: true,
-};
-console.log(student.age );
+// console.log(" a++ =",++a);
+// const student ={
+    // name:"nancy",
+    // age: 28,
+    // cgpa :8.5,
+    // ispass: true,
+// };
+
+// console.log(student.age );
+ 
+// console.log(" a++ =",a++);
+if(age<= 18){
+console.log( "verified");
+}
